71b54f98c441c6530edcd9d552cdfed37bab3bd3
ZiroReq <230985907+ZiroReq@users.noreply.github.com>
2026-09-14 15:49:26 +0700

    Updated Coinconverter

--- src/Week1/CoinConverter.java
+++ src/Week1/CoinConverter.java
@@ -2,7 +2,20 @@ package Week1;
 
 public class CoinConverter {
     static void main(){
+        int totalxu = 483274;
         System.out.println("483274 xu converts into:");
+        int quan = totalxu / 10000;
+
+        int remainingXu = totalxu % 10000;
+
+        int dong = remainingXu / 100;
+
+        remainingXu = remainingXu % 100;
+
+        int hao = remainingXu / 10;
+        remainingXu = remainingXu % 10;
+
+        System.out.println(quan + " quan, " + dong + " dong, " + hao + " hao, " + remainingXu + " xu");
 
     }
 }
